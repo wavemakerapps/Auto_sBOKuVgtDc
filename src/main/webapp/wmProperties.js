89ab367f521794df64e2391332fc674dc4a8775d
ZMlWxmArSN <automate1@wavemaker.com>
2017-11-27 22:36:52 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_sBOKuVgtDc",
+  "homePage" : "Main",
+  "name" : "Auto_sBOKuVgtDc",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
